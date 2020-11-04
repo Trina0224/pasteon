c5b9b1f9fb4e666c7a48fdf77c446cac06100a8f
Trina Shih <trina.shih@gmail.com>
2020-11-04 13:38:19 -0800

    update legal wordings.

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # pasteon
 
 
-1. multiple same link issue.seems--ok.
+1. multiple same link issue.seems--ok.  
 2. wording.  
-3. delete local images.  
+3. delete local images.  --ok. with fs.unlinkSync()  
 4. Error message handling.seems--ok.
+5. QRCODE, paypal/Venmo, ad???  
+6. Take out all console.log()
--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 3000;
 
 
 const homeStartingContent = "Click the link below and copy-paste anythin you want to keep it a while. The system will delete it after you read it or a while.";
-const aboutContent = "aboutContent";
+const aboutContent = "WARNING";
 const contactContent = "The photo only accept less than 16MB, if you upload a photo which is larger than 16MB, you will lost it. The system will delete this file after a while. Please download it in the grace period.";
 const errorContent = "The page already removed because expired or wrong link address. You could go back to homepage and reuse this link.";
 let homeStartingImportantMsg = "";
@@ -191,6 +191,13 @@ app.post('/', upload.single('image'), (req, res, next) => {
                 ;//res.redirect('/');
               }
             });
+            //delete local file.
+            try {
+              fs.unlinkSync(path.join(__dirname + '/public/img/' + req.file.filename));
+              //file removed
+            } catch(err) {
+              console.error(err)
+            }
 
           }
         } else {
@@ -203,23 +210,6 @@ app.post('/', upload.single('image'), (req, res, next) => {
 
 });
 
-// app.get("/compose", function(req, res){
-//   res.render("compose");
-// });
-//
-// app.post("/compose", function(req, res){
-//   const post = new Post({
-//     title: req.body.postTitle,
-//     content: req.body.postBody
-//   });
-//
-//
-//   post.save(function(err){
-//     if (!err){
-//         res.redirect("/");
-//     }
-//   });
-// });
 
 app.get("/about", function(req, res) {
   res.render("about", {
